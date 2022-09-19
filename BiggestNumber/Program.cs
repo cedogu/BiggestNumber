25f2f6c9a9afd234261ce2f73af5a04033bcfd01
cedogu <86116050+cedogu@users.noreply.github.com>
2022-09-19 23:12:57 +0300

    Add project files.

---
+++ BiggestNumber/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace BiggestNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 20;
+            int number2 = 30;
+            int number3 = 10;
+            int result;
+
+            result = number3;
+
+
+            if (number3 > number1)
+            {
+                result = number3;
+            }
+            else if (number1 > number2)
+            {
+                result = number1;
+            }
+            else
+            {
+                result = number2;
+            }
+
+            // or we can do in this way as well;
+
+            
+            //if (number1 > number3)
+            //{
+            //    result = number1;
+            //}
+
+            //if (number2 > number3)
+            //{
+            //    result = number2;
+            //}
+
+
+            Console.WriteLine("The biggest number is: " + result);
+
+            Console.Read();
+
+        }
+    }
+}
